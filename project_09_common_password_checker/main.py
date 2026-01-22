d3e63d821e19baa070ff159d5482d2055ef51e6c
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2026-01-22 08:13:18 +0000

    fix: review project 09

--- project_09_common_password_checker/main.py
+++ project_09_common_password_checker/main.py
@@ -1,9 +1,22 @@
 def check_password(password: str) -> str:
+
+    # with ... as ...:
+    #   works with context managers
+    #       context manager is any object that defines:
+    #           __enter__() -> returns value used in "as ..."
+    #               (set up context)
+    #           __exit__()
+    #               (clean up context)
     with open("passwords.text") as file:
         common_passwords: list[str] = file.read().splitlines()
-        for i, common_password in enumerate(common_passwords, start=1):
-            if common_password == password:
-                return f'"{password}": ❌ password is common (#{i})'
+
+    # the scope of common_passwords is check_password function
+    #   - if, for, with, etc. do not create a new scope
+    #   - Only functions, classes, and modules create scopes
+    for i, common_password in enumerate(common_passwords, start=1):
+        if common_password == password:
+            return f'"{password}": ❌ password is common (#{i})'
+
     return f'"{password}": ✅ password is unique'
 
 
